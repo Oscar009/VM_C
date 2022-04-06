a7014df794e2ec53bf5e5fd5dcfaa7b58ed4243e
Oscar Ortiz <oortizbarba@gmail.com>
2022-04-05 19:07:56 -0500

    base for commands and create virtual machine

--- component.cc
+++ component.cc
@@ -8,5 +8,12 @@ component::component()
 void component::showHello(string msj)
 {
     cout << msj << endl;
+    int delay = 5;
+
+    delay *= CLOCKS_PER_SEC;
+
+    clock_t now = clock();
+
+    while (clock() - now < delay);
     system("ls");
 }
